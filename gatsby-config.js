9510637179c5b23f7f64ff136fede9c6bb87985e
lukekeum <lukekeum11@gmail.com>
2021-05-10 01:34:41 +0900

    feat(plugin): Add plugin for load markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,14 @@ module.exports = {
         implementation: require("node-sass"),
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
