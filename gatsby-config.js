233ff1fffe80488e761d15b196285769a491c0c5
lukekeum <lukekeum11@gmail.com>
2021-05-10 22:34:27 +0900

    feat(plugin): Add reading time gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,12 @@ module.exports = {
         path: `${__dirname}/posts`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-reading-time`],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
